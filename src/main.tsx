4d00f6f42e996a6fe2c5e85f9616ff79ad5551b0
tsaeeeee <donaldewuh@gmail.com>
2026-08-22 20:14:58 +0700

    load workspace skin after mixer skin

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import './mixer.css';
+import './workspace.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
