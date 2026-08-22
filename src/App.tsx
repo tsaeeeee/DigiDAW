778f4a3c26e249969c584b0eafc2976807d6f032
tsaeeeee <donaldewuh@gmail.com>
2026-08-23 02:46:57 +0700

    wire timeline clip multi selection

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import AppBase from './AppBase';
+import { MultiSelectBehavior } from './components/MultiSelectBehavior';
 import { EditingOverlay } from './components/EditingOverlay';
 import { ZoomWheelBehavior } from './components/ZoomWheelBehavior';
 import { BrandLogoBehavior } from './components/BrandLogoBehavior';
@@ -8,6 +9,7 @@ export default function App() {
   return (
     <>
       <AppBase />
+      <MultiSelectBehavior />
       <EditingOverlay />
       <ZoomWheelBehavior />
       <BrandLogoBehavior />
