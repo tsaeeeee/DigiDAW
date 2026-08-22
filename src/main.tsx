81ebdb69c8acb0017be85c1398900ed72438c0e5
tsaeeeee <donaldewuh@gmail.com>
2026-08-22 18:19:16 +0700

    load compact flat mixer skin

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import './mixer.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
